5fd4d48b747a01e3e18d963bcecbef167bcdb7be
Fang <saij84@gmail.com>
2021-05-15 09:55:11 +1000

    refactor code and add documentation

--- autofillWordDoc/autoFill.py
+++ autofillWordDoc/autoFill.py
@@ -1,28 +1,51 @@
+"""
+Automating excel to word
+"""
+
 import pandas as pd
 from docxtpl import DocxTemplate
 from officeAutomation.autofillWordDoc.constants import constants as CONST
 
-data = pd.read_excel(CONST.INPUTDATAFILENAME)
-pdData = pd.DataFrame(data)
 
 def replaceSpace(inputStr):
+    """
+    Replace any space character with '_'
+    :param inputStr: string
+    :return: strin
+    """
     if ' ' in inputStr:
         inputStr = inputStr.replace(' ', '_')
     return inputStr
 
-def buildContext(idx):
-    context = dict()
-    for columName, row in pdData.iteritems():
-        if 'Unnamed' in columName:
-            continue
-        newColumName = replaceSpace(columName)
-        context.update({newColumName: row[idx]})
-    return context
 
-for idx in range(len(pdData)):
-    doc = DocxTemplate(CONST.WORDTEMPLATEFILENAME)
-    context = buildContext(idx)
-    renders = doc.render(context)
-    itemIDX = context['id']
-    savePath = '{}\\{}.docx'.format(CONST.OUTPUTSPATH, itemIDX)
-    doc.save(savePath)
+def autoFill(inputExcelFile, templateWordFile):
+    """
+    auto fill a docx template
+    :param inputExcelFile: xlsx or xls file
+    :param templateWordFile: docx file
+    :return: None
+    """
+    data = pd.read_excel(inputExcelFile)
+    pdData = pd.DataFrame(data)
+    numIDX = len(pdData.index)
+
+    for idx in range(numIDX):
+        doc = DocxTemplate(templateWordFile)
+
+        context = dict()
+        for columName, row in pdData.iteritems():
+            if 'Unnamed' in columName:
+                continue
+            newColumName = replaceSpace(inputStr=columName)
+            context.update({newColumName: row[idx]})
+        doc.render(context)
+
+        itemIDX = context['id']
+        savePath = '{}\\{}.docx'.format(CONST.OUTPUTSPATH, itemIDX)
+        doc.save(savePath)
+
+
+inputExcel = '{}\\{}'.format(CONST.INPUTSPATH, 'inputData.xlsx')
+wordTemplate = '{}\\{}'.format(CONST.TEMPLATES, 'wordTemplate.docx')
+
+autoFill(inputExcelFile=inputExcel, templateWordFile=wordTemplate)
--- autofillWordDoc/constants/constants.py
+++ autofillWordDoc/constants/constants.py
@@ -7,6 +7,3 @@ ROOTPATH = '\\'.join(_currentFilePathSplit)
 OUTPUTSPATH = '{}\\{}'.format(ROOTPATH, 'outputs')
 INPUTSPATH = '{}\\{}'.format(ROOTPATH, 'inputs')
 TEMPLATES = '{}\\{}'.format(ROOTPATH, 'templates')
-
-INPUTDATAFILENAME = '{}\\{}'.format(INPUTSPATH, 'inputData.xlsx')
-WORDTEMPLATEFILENAME = '{}\\{}'.format(TEMPLATES, 'wordTemplate.docx')
